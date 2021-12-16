ce6c2cb85ec955a6a5ade6437be63d84ac3eb5b6
alameluDeepika <91462351+alameluDeepika@users.noreply.github.com>
2021-12-16 17:47:10 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){ } 
+function setup(){ var canvas=createCanvas(350,350);
+canvas.center(); 
+video=createCapture(VIDEO); 
+video.hide();
+video.size(350,350); 
+poseNet=ml5.poseNet(video,modelLoaded); 
+poseNet.on('pose',gotPoses); } 
+function modelLoaded(){ console.log("model is loaded"); } 
+function gotPoses(results){ 
+if(results.length > 0){ 
+console.log(results); } 
+} 
+function draw(){ } 
+function take_snapshot(){ 
+save("clownfilter.png"); 
+}
